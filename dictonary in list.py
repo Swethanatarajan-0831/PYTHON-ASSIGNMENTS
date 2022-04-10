a4a44a60adbeb0e60b5a4b9a23f0c59069bc3cbf
Swethanatarajan-0831 <102980606+Swethanatarajan-0831@users.noreply.github.com>
2022-04-10 13:54:36 +0530

    Add files via upload

---
+++ dictonary in list.py
@@ -0,0 +1,4 @@
+dict1={'name':'swetha natarajan','age':18}
+values=dict1.values()
+list1=list(values)
+print(list1)
---
+++ list in dictonary.py
@@ -0,0 +1,3 @@
+dict1={'color':['blue','green','red'],'month':['june','july','august']}
+print("color=",dict1['color'][0])
+print('month=',dict1['month'][2])
